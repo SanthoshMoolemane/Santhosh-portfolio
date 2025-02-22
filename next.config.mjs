bb9446aa30d3a1dd24088e9e6b118fab4615349e
Santhosh Moolemane <santhoshmbitsme@gmail.com>
2025-02-22 23:34:10 +0530

    Attempt 7

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    distDir: "out",
-    trailingSlash: true,
-    basePath: "/Santhosh-portfolio",
-    assetPrefix: "/Santhosh-portfolio",
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  export default nextConfig;
+  output: "export",
+  distDir: "out",
+  trailingSlash: true,
+  basePath: "/Santhosh-portfolio",
+  assetPrefix: "/Santhosh-portfolio/",
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.output.publicPath = "/Santhosh-portfolio/_next/";
+    return config;
+  },
+};
+
+export default nextConfig;
